35996ffcf6746800defe18750bbb9c00f85442f5
PRATISHTHA-BHOITE <43807178+PRATISHTHA-BHOITE@users.noreply.github.com>
2022-08-21 01:23:43 +0530

    Add files via upload

---
+++ src/Components/Content.js
@@ -0,0 +1,210 @@
+import React from 'react';
+
+  function Content() {
+    return (
+       <div>
+         <section class="">
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <img class="d-block w-100" src="https://media.istockphoto.com/photos/taj-mahal-front-view-reflected-on-the-reflection-pool-picture-id962421230?b=1&k=20&m=962421230&s=170667a&w=0&h=0FzUcDlk4JHReGc-KDmpzjmHSDMc3KX7OPRqELdz53g=" alt="First slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100 " src="https://media.istockphoto.com/photos/happy-gypsy-indian-girl-using-laptop-india-picture-id1357765612?b=1&k=20&m=1357765612&s=170667a&w=0&h=KuAl6GU2YcxR_vhlhO6A0GGyAAgERoDnbSVYvjzeKEo=" alt="Second slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100" src="https://media.istockphoto.com/photos/holy-town-varanasi-and-the-river-ganges-picture-id827065008?b=1&k=20&m=827065008&s=170667a&w=0&h=YjToqYIXDJUZvgVFjW8K_WSPAODbZhwRPNcO8SjGo14=" alt="Third slide" style={{height:"auto"}}/>
+                  </div>
+                </div>
+               </div>
+        </section>   
+
+        <section class="bg-light" id="about">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">Know about us</h3>
+               </div>
+              </div>
+             <div class="row">
+                <p class="mt-4 mb-5">Welcome to land of holy temples, soaring snow-capped peaks of the 
+                Himalayas, the desert state of Rajasthan, the lush rainforests of South India, the tropical
+                 beaches of Goa,Kerala, Karnataka, and other states, and the jungles of central India,
+                 where tigers roam.Welcome to Incrediable India!!
+                 We are here as your personal travel guide! DM us to book your dream Holiday!
+         </p>
+             </div>   
+          </div>   
+        </section>   
+
+        <section class="" id="destinations">   
+         <div class="container">
+             <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">Favourite Destinations</h3>
+               </div>
+              </div>
+              <div class="row">
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="https://media.istockphoto.com/photos/selfie-point-with-beautiful-backdrop-of-sahyadri-mountains-at-dhodap-picture-id1335855873?b=1&k=20&m=1335855873&s=170667a&w=0&h=iLcwMMbbcNgYZSqWq_AWCfpze1pOn-co4jyiX-BL5i4=" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Maharashtra</h4>
+                          <p class="card-text text-secondary">With an incredible mashup of Sahyadri Ranges,Konkan Coast, Forts ,Tadoba National Park
+                           there's is no better place to go for a holiday in India than Maharashtra.
+                           The state is primarily known for its ancient Ellora and Ajanta caves, and pilgrim destinations. Plan a holiday in Maharashtra to witness the dazzling range of experience,
+                           beautiful landscapes and years of history.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="https://media.istockphoto.com/photos/backwaters-of-kerala-picture-id472909442?b=1&k=20&m=472909442&s=170667a&w=0&h=nhg8sAdDcECDO7wQvuzFA6O09feIngFAzGZyF86oKvA=" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Kerala</h4>
+                          <p class="card-text text-secondary">Adorned with the emerald backwaters, sprawling and lush tea estates, pristine hill stations, and azure Arabian Sea, 
+                          Kerala tourism boasts numerous tourist places with which you will fall in love instantly.
+                          In North Kerala, districts like Kasaragod, Kannur, Wayanad, Kozhikode, and Malappuram have 
+                          many attractions for the tourists. Wayanad offers a lovely hill station holiday.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="https://media.istockphoto.com/photos/beautiful-lake-picture-id1218830644?b=1&k=20&m=1218830644&s=170667a&w=0&h=hHSQcCYj_CZvfAet3mfoqfDJ_Zr1DBHimKdys50JQcA=" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Ladakh</h4>
+                          <p class="card-text text-secondary">Places like Tso Moriri, Nubra Valley, Pangong Tso, Siachen Glacier, Shanti Stupa in Leh and Ladakh gives beautiful travel experience. 
+                          The magnificent mountains and mesmerising lakes will take your breath away. This beautiful region has a diverse topography: lush green valleys and glacial lakes, rustic villages. 
+                          It lets you escape your busy life and is the perfect destination to rejuvenate yourself.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Find Out More!</a>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+        </div>
+       </section>  
+
+
+       <section class=""  id="places">
+        <div class="container">
+          <div class="col-sm-12 col-md-12 mb-4">
+             <h3 class="text-center text-secondary mt-4">Places and Description</h3>
+         </div>
+       <div class="accordion" id="accordionExample">
+        <div class="card">
+         <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+               Tourist Places
+            </button>
+            </h2>
+         </div>
+
+         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            Goa, Mcleodganj, Srinagar, Andaman, Leh-Ladak, Binsar, Coorg, Kerala, Kasol, Kutch, Bir, 
+            Billing, Assam, Rishikesh, Shimla, Tirthan Valley, Jim Corbett, Manali, Udaipur, Auli, Mysore, 
+            Valley Of Flowers, Jaisalmer, Jodhpur, Parashar Lake, Mukteshwar, Dhanaulti, Varanasi, Mumbai,
+            Kolkata, Delhi, Meghalaya, Sikkim.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Best Destination
+            </button>
+            </h2>
+         </div>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+            Manali - popular hill stations in Himachal, Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges.<br></br>
+            Goa - known for its endless beaches, stellar nightlife, eclectic seafood, world-heritage listed architecture.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+               Best Heritages
+            </button>
+            </h2>
+         </div>
+         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+            <ul>Qutub Minar, Delhi</ul><br/>
+            <ul>Churches and Convents, Goa</ul><br/>
+            <ul>Hampi, Karnataka</ul><br/>
+            <ul>Ajanta Caves, Maharashtra</ul><br/>
+            </div>
+         </div>
+      </div>
+      </div>
+    </div> 
+    </section>
+
+    
+    <section class="bg-light mt-5" id="adventure">    
+     <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h3 class="text-center mt-4 text-secondary">Adventure activities</h3>
+         </div>
+        <div class="col-md-4">
+          <div class="testimonial mb-5">
+           <div class="avatar mx-auto">
+            <img src="https://media.istockphoto.com/photos/scuba-diving-underwater-scuba-diver-in-blue-lagoon-picture-id598247070?b=1&k=20&m=598247070&s=170667a&w=0&h=LTh-mdGVBmg3EBWAVAPr7szemoOpOz2NOOOIazJiMVk=" class="rounded-circle z-depth-1 img-fluid" height={600} width={600}/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Scuba diving </h4>
+          <h6 class="font-weight-bold blue-text my-3">Konkan</h6>
+          <p class="font-weight-normal dark-grey-text">
+          Scuba diving is a mode of underwater diving whereby divers use breathing equipment 
+          that is completely independent of a surface air supply.</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://sportsmatik.com/uploads/matik-sports-corner/matik-know-how/paragliding_1493810301_97866.jpg" class="rounded-circle z-depth-1 img-fluid" height={300} width={300}/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Paragliding</h4>
+          <h6 class="font-weight-bold blue-text my-3">Bir Billing</h6>
+          <p class="font-weight-normal dark-grey-text">
+          Paragliding is the competitive adventure sport of flying paragliders: 
+          lightweight, free-flying, foot-launched glider aircraft with no rigid primary structure. </p>
+        </div>
+        </div>
+
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmAbWukVubS1xiTNE4O5uEHDYOUdwYoDWQHg&usqp=CAU" class="rounded-circle z-depth-1 img-fluid" height={300} width={300} ></img>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Kayaking</h4>
+          <h6 class="font-weight-bold blue-text my-3">Karnataka</h6>
+          <p class="font-weight-normal dark-grey-text">Flat-water kayaking takes place in rivers, lakes and the ocean.
+          It allows the boat driver to maneuver through waterways by sitting face-forward and propelling ahead with alternating side-to-side paddle strokes.</p>
+        </div>
+
+      </div>
+      
+      </div>
+    </div>
+
+   </section> 
+   </div>
+    );
+  }
+  
+  export default Content;
